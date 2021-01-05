9bc0d8926ee565654bbcac35881238c901005ad8
Angela Cai <angelacai@Acais-Mac.fios-router.home>
2021-01-04 21:27:02 -0500

    tested simple pig latin

--- PigLatin.java
+++ PigLatin.java
@@ -1,22 +1,29 @@
 public class PigLatin {
 
+    public static void main(String[] args) {
+        String[] words = {"mock", "pie", "david", "aaron"};
+        for (String word: words){
+            System.out.println(pigLatinSimple(word));
+        }
+
+    }
+
     public static String pigLatinSimple(String s){
         char[] vowels = {'a', 'e', 'i', 'o', 'u'};
-        result = "";
+        String result = "";
         for (int i = 0; i < vowels.length; i++) {
             if (s.charAt(0) == vowels[i]) {
-                result = s + "hay"
-            }
-            else {
-                result = s.substring(1, s.length+1) + s.charAt(0) + "ay";
+                result = s + "hay";
             }
         }
+
+        if (result == "") result = s.substring(1, s.length()) + s.charAt(0) + "ay";
         return result;
     }
 
-    public static String pigLatin(String s){
-
-
-    }
+    // public static String pigLatin(String s){
+    //
+    //
+    // }
 
 }
