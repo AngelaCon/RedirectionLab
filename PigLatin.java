0ba9a3ff63287782ff1c0f3273c5be9ca476d345
Angela Cai <angelacai@Acais-Mac.fios-router.home>
2021-01-04 21:37:00 -0500

    tested pigLatin

--- PigLatin.java
+++ PigLatin.java
@@ -1,14 +1,14 @@
 public class PigLatin {
 
     public static void main(String[] args) {
-        String[] words = {"mock", "pie", "david", "aaron"};
+        String[] words = {"mock", "pie", "david", "aaron", "the", "check", "skee", "emu", "grade"};
         for (String word: words){
-            System.out.println(pigLatinSimple(word));
+            System.out.println(pigLatin(word));
         }
-
     }
 
     public static String pigLatinSimple(String s){
+        s = s.toLowerCase();
         char[] vowels = {'a', 'e', 'i', 'o', 'u'};
         String result = "";
         for (int i = 0; i < vowels.length; i++) {
@@ -21,9 +21,17 @@ public class PigLatin {
         return result;
     }
 
-    // public static String pigLatin(String s){
-    //
-    //
-    // }
+    public static String pigLatin(String s){
+        String output = "";
+        String[] diagraphs = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+        for (int i = 0; i < diagraphs.length; i++) {
+            if (s.substring(0,2).equals(diagraphs[i])) {
+                output = s.substring(2,s.length()) + s.substring(0,2) + "ay";
+            }
+        }
+        if (output == "") output = pigLatinSimple(s);
+        return output;
+
+    }
 
 }
