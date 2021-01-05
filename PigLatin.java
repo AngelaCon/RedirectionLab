1f93c430378c18d19d1e82b25dbbaccde1cf71c3
Angela Cai <angelacai@Acais-Mac.fios-router.home>
2021-01-04 22:06:36 -0500

    tested bestPigLatin

--- PigLatin.java
+++ PigLatin.java
@@ -1,11 +1,21 @@
 public class PigLatin {
 
     public static void main(String[] args) {
-        String[] words = {"mock", "pie", "david", "aaron", "the", "check", "skee", "emu", "grade"};
+        String result = "";
+        String[] words = {"mock", "pie", "david", "aaron", "the", "check", "skee", "emu", "grade", "*emu", "4chan", "fish!", "fish", "the.", "cat!", "amazing?", "apple%"};
+        String[] correctWords = {"ockmay", "iepay", "avidday", "aaronhay", "ethay", "eckchay", "eeskay", "emuhay", "adegray", "*emu", "4chan", "ishfay!", "ishfay", "ethay.", "atcay!", "amazinghay?", "applehay%"};
         for (String word: words){
-            System.out.println(pigLatin(word));
+            for (String correctWord: correctWords) {
+                if (pigLatinBest(word).equals(correctWord)) result = "all correct cutie";
+                else {
+                    result= "error at" + word;
+                }
         }
     }
+    System.out.println(result);
+}
+
+
 
     public static String pigLatinSimple(String s){
         s = s.toLowerCase();
@@ -22,6 +32,7 @@ public class PigLatin {
     }
 
     public static String pigLatin(String s){
+        s = s.toLowerCase();
         String output = "";
         String[] diagraphs = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
         for (int i = 0; i < diagraphs.length; i++) {
@@ -31,7 +42,22 @@ public class PigLatin {
         }
         if (output == "") output = pigLatinSimple(s);
         return output;
-
     }
+
+    public static String pigLatinBest(String s){
+        s = s.toLowerCase();
+        String result = "";
+        char firstLetter = s.charAt(0);
+        char lastLetter = s.charAt(s.length()-1);
+        if (!(Character.isLetter(firstLetter))) result = s;
+        else if (!(Character.isLetter(lastLetter)) && !(Character.isDigit(lastLetter))) {
+            result += pigLatin(s.substring(0,s.length()-1)) + lastLetter;
+        }
+        else {
+            result += pigLatin(s);
+        }
+        return result;
+    }
+
 
 }
